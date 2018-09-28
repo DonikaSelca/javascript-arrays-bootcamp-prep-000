5719d1cafcdeb125645db3adea3654b8a90338c6
dselca <dselca@gmail.com>
2018-09-28 19:13:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -44,6 +44,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  newArray = array.slice()
+  newArray = array.slice(0, array.length)
   return newArray
 }
